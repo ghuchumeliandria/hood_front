1ff1010d810dd63efea7b8efe26fabff1881b1af
Andria Ghurtchumelia <121307641+ghuchumeliandria@users.noreply.github.com>
2025-12-01 00:17:30 +0400

    ravi

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
-  return (
-    <div>
-    <h1>Hello boslevi</h1>
-    </div>
-  )
+    return (
+        <div>
+            <Image src={"https://media.tenor.com/0rKL5dtMPkgAAAAe/monkey.png"} alt='monkey' width={200} height={200} />
+            <h1>ravqna jer meti araferia</h1>
+        </div>
+    )
 }
