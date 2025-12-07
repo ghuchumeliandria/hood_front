00552c8ae13855cbb622c83d28bbde97ca976ad5
Andria Ghurtchumelia <121307641+ghuchumeliandria@users.noreply.github.com>
2025-12-08 00:41:54 +0400

    install new version

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -8,6 +8,7 @@ export default function page() {
             <div>
                 <Image src={"https://media.tenor.com/0rKL5dtMPkgAAAAe/monkey.png"} alt='monkey' width={200} height={200} />
                 <h1>ravqna jer meti araferia</h1>
+                <h2>uceb ragaca davamate da test</h2>
             </div>
         </ IsLoggedIn >
     )
